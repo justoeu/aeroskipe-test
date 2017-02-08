f7b798a65bdef954530a0a11a9315304b8a5ad73
Valmir Robson Justo <justoeu@gmail.com>
2017-02-08 11:41:01 -0200

    Fist Commit

---
+++ src/main/java/br/com/justoeu/aerospike/AeroskipeTestApplication.java
@@ -0,0 +1,12 @@
+package br.com.justoeu.aerospike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeroskipeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeroskipeTestApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/justoeu/aerospike/AeroskipeTestApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.justoeu.aerospike;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AeroskipeTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
